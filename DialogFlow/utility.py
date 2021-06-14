f8560ac1fb9801266eab5255bdc4d7a97a394f87
Avish <porwalavish@gmail.com>
2021-06-14 21:58:39 +0530

    Added text proccessing

--- DialogFlow/utility.py
+++ DialogFlow/utility.py
@@ -1,6 +1,10 @@
+from urllib import request
 from google.cloud import dialogflow
 from django.http import HttpResponse, JsonResponse, response
 import json
+import nltk
+from nltk.tokenize import sent_tokenize
+#nltk.download('punkt') Download once, then comment it!
 
 class DialogFlow:
     def __init__(self, project_id, session_id,language_code):
@@ -8,33 +12,49 @@ class DialogFlow:
         self.session_id=session_id
         self.language_code=language_code
 
-    def getSuggestion(self,text):
+    def get_suggestions(self,text):
         return HttpResponse(self.detect_intent_texts(text))
 
-    def detect_intent_texts(self,texts):
+    def detect_intent_texts(self,text):
     
         session_client = dialogflow.SessionsClient()
 
         session = session_client.session_path(self.project_id, self.session_id)
         print("Session path: {}\n".format(session))
+        suggestions=[]
+        for texts in text:
+            if(len(texts)>256):
+                continue
+            text_input = dialogflow.TextInput(text=texts, language_code=self.language_code)
 
-        
-        text_input = dialogflow.TextInput(text=texts, language_code=self.language_code)
+            query_input = dialogflow.QueryInput(text=text_input)
 
-        query_input = dialogflow.QueryInput(text=text_input)
-
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-
-        print("=" * 20)
-        print("Query text: {}".format(response.query_result.query_text))
-        print(
-            "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
+            response = session_client.detect_intent(
+                request={"session": session, "query_input": query_input}
             )
-            )
-        
-        send={"suggestion":response.query_result.fulfillment_text}
+
+            print("=" * 20)
+            print("Query text: {}".format(response.query_result.query_text))
+            print(
+                "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+                )
+                )
+            suggestions.append(response.query_result.fulfillment_text)
+        send={"suggestion":suggestions}
         return json.dumps(send,indent=4)    
+
+class TextCleaning():
+    def __init__(self,text):
+        self.text=text
+    def remove_extra_spaces(self):
+        self.text=" ".join(self.text.split()) 
+    def to_sentence(self):
+        texts=sent_tokenize(self.text)
+        return texts
+    def preprocess_text(self):
+        self.remove_extra_spaces()
+        return self.to_sentence()  
+
+          
--- DialogFlow/views.py
+++ DialogFlow/views.py
@@ -4,14 +4,17 @@ from django.http import HttpResponse, JsonResponse, response
 from django.views.decorators.csrf import csrf_exempt
 import json,uuid,os
 from .utility import DialogFlow
-
+from .utility import TextCleaning
 
 def helper(text):
+
     project_id=os.getenv('PROJECT_ID')
     session_id=uuid.uuid1()
     language_code="en-US"
-    Object=DialogFlow(project_id,session_id,language_code)
-    return Object.getSuggestion(text)
+    dialog_flow=DialogFlow(project_id,session_id,language_code)
+    cleaned_text=TextCleaning(text)
+    texts=cleaned_text.preprocess_text()
+    return dialog_flow.get_suggestions(texts)
 
 
 
@@ -24,7 +27,7 @@ def get_suggestion(request):
         text=obj['note']
         if(len(text)==0):
             return HttpResponse(status=400)
-        return helper(text);   
+        return helper(text)  
          
     return HttpResponse(status=405)
 
